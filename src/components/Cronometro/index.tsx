7260cd8a539161412c0eb34c69e0c042ca149f4c
LenaMoromisato <moromisato.lena@gmail.com>
2023-07-06 16:11:18 -0300

    mostrando no relógio

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -21,9 +21,8 @@ export default function Cronometro({ selecionado }: Props) {
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
-            Tempo: {tempo}
         <div className={style.relogioWrapper}>
-            <Relogio />
+            <Relogio tempo={tempo} />
         </div>
         <Botao>
             Começar!
